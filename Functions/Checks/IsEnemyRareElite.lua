408a9f36973635afe7dacc55a20c233d1d2ff34f
HerrHaseGermany <99294728+HerrHaseGermany@users.noreply.github.com>
2026-02-06 12:24:57 +0100

    fix
    - fix enemies slain counted twice
    - fix elite enemies not counted

--- Functions/Checks/IsEnemyRareElite.lua
+++ Functions/Checks/IsEnemyRareElite.lua
@@ -10,14 +10,36 @@ function IsEnemyRareElite(unitGUID)
     end
   end
 
-  for i = 1, GetNumGroupMembers() do
-    local unitID = 'party' .. i .. 'target'
-    if UnitGUID(unitID) == unitGUID then
-      local classification = UnitClassification(unitID)
-      if classification == 'rareelite' then
-        return true
+  if IsInGroup() then
+    if IsInRaid() then
+      for i = 1, GetNumGroupMembers() do
+        local unitID = 'raid' .. i .. 'target'
+        if UnitGUID(unitID) == unitGUID then
+          local classification = UnitClassification(unitID)
+          if classification == 'rareelite' then
+            return true
+          end
+        end
+      end
+    else
+      for i = 1, GetNumGroupMembers() do
+        local unitID = 'party' .. i .. 'target'
+        if UnitGUID(unitID) == unitGUID then
+          local classification = UnitClassification(unitID)
+          if classification == 'rareelite' then
+            return true
+          end
+        end
       end
     end
+  end
+
+  if EnemyClassificationCache and EnemyClassificationCache.RefreshFromUnits then
+    EnemyClassificationCache.RefreshFromUnits()
+  end
+  local cached = EnemyClassificationCache and EnemyClassificationCache.Get and EnemyClassificationCache.Get(unitGUID)
+  if cached == 'rareelite' then
+    return true
   end
 
   return false
